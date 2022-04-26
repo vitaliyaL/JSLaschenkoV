bc7cda7131d4d66fdab40fd0d27ceadf0009feea
vitaliyaL <vita0623@mail.ru>
2022-04-26 17:20:36 +0300

    task3 added

--- script/app.js
+++ script/app.js
@@ -7,12 +7,39 @@ if (message) {
   console.log("");
 }
 //Задание2////////////////////////////////////////////////////////
-let num1 = +prompt("Задание 2 \nВведите первое число:");
-let num2 = +prompt("Введите второе число:");
-let num3 = +prompt("Введите третье число:");
-if (num1 && num2 && num3) {
-  alert(num1 + num2 + num3);
+let num1 = prompt("Задание 2 \nВведите первое число:");
+if (num1) {
+  let num2 = prompt("Введите второе число:");
+  if (num2) {
+    let num3 = prompt("Введите третье число:");
+    if (num3) {
+      if (
+        !isNaN(+num1) &&
+        !isNaN(+num2) &&
+        !isNaN(+num3)
+      ) {
+        alert(+num1 + +num2 + +num3);
+      } else {
+        alert("Данные введены некорректно!");
+      }
+    } else {
+      console.log('');
+    }
+  } else {
+    console.log('');
+  }
+} else {
+  console.log("");
 }
-else{
-  alert("Вы ввели не все данные!");
+//Задание3////////////////////////////////////////////////////////
+let userName = prompt("Задание 3 \nВведите ваше имя:");
+if (userName) {
+  let age = +prompt("Введите ваш возраст:");
+  if (age >= 18) {
+    alert("Добро пожаловать, " + userName + "!");
+  } else {
+    alert("Извините, " + userName + ", доступ закрыт!");
+  }
+} else {
+  console.log("");
 }
