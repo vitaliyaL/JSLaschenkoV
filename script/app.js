4ae7f60ef5757f9c0f88e92076886613c48dc02e
vitaliyaL <vita0623@mail.ru>
2022-04-26 17:56:35 +0300

    task4_added

--- script/app.js
+++ script/app.js
@@ -13,20 +13,16 @@ if (num1) {
   if (num2) {
     let num3 = prompt("Введите третье число:");
     if (num3) {
-      if (
-        !isNaN(+num1) &&
-        !isNaN(+num2) &&
-        !isNaN(+num3)
-      ) {
+      if (!isNaN(+num1) && !isNaN(+num2) && !isNaN(+num3)) {
         alert(+num1 + +num2 + +num3);
       } else {
         alert("Данные введены некорректно!");
       }
     } else {
-      console.log('');
+      console.log("");
     }
   } else {
-    console.log('');
+    console.log("");
   }
 } else {
   console.log("");
@@ -42,4 +38,36 @@ if (userName) {
   }
 } else {
   console.log("");
+}
+//Задание4////////////////////////////////////////////////////////
+let confBlock = confirm("Задание 4 \nХотите ввести название блока?");
+if (confBlock) {
+  let promBlock = prompt("Введите название блока:");
+  if (promBlock) {
+    let confEl = confirm("Хотите ввести название элемента?");
+    if (confEl) {
+      let promEl = prompt("Введите название элемента:");
+      if (promEl) {
+        let confMod = confirm("Хотите ввести название модификатора?");
+        if (confMod) {
+          let promMod = prompt("Введите название модификатора:");
+          if (promMod) {
+            confirm(promBlock +'_'+ promEl +'_'+ promMod);
+          } else {
+            confirm(promBlock +'_'+ promEl);
+          }
+        } else {
+          confirm(promBlock +'_'+ promEl);
+        }
+      } else {
+        confirm(promBlock);
+      }
+    } else {
+      confirm(promBlock);
+    }
+  } else {
+    confirm("Элементу не присвоен класс.");
+  }
+} else {
+  confirm("Элементу не присвоен класс.");
 }
